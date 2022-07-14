d98fc158fb83d12ebbce76e38f17f97034d7635c
YordanMarkov <yordanmarkov2004@gmail.com>
2022-07-14 16:02:40 +0300

    Day 9

--- Classwork/Sofia Airport/4sql.sql
+++ Classwork/Sofia Airport/4sql.sql
@@ -1,4 +1,4 @@
-ALTER PROCEDURE from_to (@StartDest varchar(50), @EndDest varchar(50), @isDirect bit, @Date date)
+ALTER PROCEDURE from_to (@StartDest int, @EndDest int, @isDirect bit, @Date date)
 AS
 BEGIN
 	IF @isDirect = 1
@@ -8,12 +8,22 @@ BEGIN
 			CAST(f.Duration as DATETIME), f.Price
 			FROM Flights f
 			LEFT JOIN Planes p ON f.Plane_ID = p.ID
-			WHERE f.StartDest = @StartDest AND f.EndDest = @EndDest 
+			WHERE f.StartDest_ID = @StartDest AND f.EndDest_ID = @EndDest 
 			AND @Date = CAST(f.Departure_time AS DATE)
 			GROUP BY f.ID, f.Departure_time, f.Duration, f.Price, f.[Delay]
 		END
 	IF @isDirect = 0
 		BEGIN
-			--select 'pupesh'
+			SELECT f.ID, f.Departure_time,  
+			SUM(p.Seats - f.Passengers) AS "Empty seats", f.Departure_Time + CAST(f.[Delay] as DATETIME) + 
+			CAST(f.Duration as DATETIME), f.Price
+			FROM Flights f
+			LEFT JOIN Planes p ON f.Plane_ID = p.ID
+			LEFT JOIN Direction d ON d.[From] = f.StartDest_ID
+			LEFT JOIN Flights f2 ON d.[To] = f2.StartDest_ID
+			WHERE @Date = CAST(f.Departure_time AS DATE) 
+			GROUP BY f.ID, f.Departure_time, f.Duration, f.Price, f.[Delay]
 		END
 END
+
+EXEC from_to 1, 6, 0, '2022-07-12'
